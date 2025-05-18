77a328ba134afe4077c1fe2cec5f6c61b2221198
NanbanTaro <chicken.de.nanban@gmail.com>
2025-05-18 12:47:00 +0900

    プロジェクト追加

---
+++ WidgetSampleApp/WidgetSampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  WidgetSampleApp.swift
+//  WidgetSampleApp
+//
+//  Created by NanbanTaro on 2025/05/17.
+//  
+//
+
+import SwiftUI
+
+@main
+struct WidgetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
