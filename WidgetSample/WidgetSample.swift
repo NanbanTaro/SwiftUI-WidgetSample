026d4c64d7d82e411344509c7a4b115472257ec0
NanbanTaro <chicken.de.nanban@gmail.com>
2025-05-18 16:50:39 +0900

    ベース追加

--- WidgetSample/WidgetSample.swift
+++ WidgetSample/WidgetSample.swift
@@ -10,15 +10,20 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
+    // データ取得時などに表示されるWidgetに渡されるデータを返す
+    // 所謂、Loading...など
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), emoji: "😀")
     }
 
+    // Widget選択画面でのWidgetに渡されるデータを渡す
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        let entry = SimpleEntry(date: Date(), emoji: "🖐️")
         completion(entry)
     }
 
+    // Widget選択画面で選択された後のデータを返す
+    // 次回の更新タイミングも指定する
     func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
@@ -45,16 +50,37 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct WidgetSampleEntryView : View {
+    /// Widgetの種類を取得できる
+    @Environment(\.widgetFamily) var family
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
+        switch family {
+        case .systemSmall:
+            Text("systemSmall")
+        case .systemMedium:
+            Text("systemMedium")
+        case .systemLarge:
+            Text("systemLarge")
+        case .systemExtraLarge:
+            Text("systemExtraLarge")
+        case .accessoryInline:
+            Text("accessoryInline")
+        case .accessoryCircular:
+            Text("accessoryCircular")
+        case .accessoryRectangular:
+            Text("accessoryRectangular")
+        default:
+            Text("default")
         }
+
+//        VStack {
+//            Text("Time:")
+//            Text(entry.date, style: .time)
+//
+//            Text("Emoji:")
+//            Text(entry.emoji)
+//        }
     }
 }
 
@@ -62,6 +88,9 @@ struct WidgetSample: Widget {
     let kind: String = "WidgetSample"
 
     var body: some WidgetConfiguration {
+        // 編集なしのwidgetを追加
+        // kind: 複数のWidgetを追加できる(アプリ起動からも識別可能)
+        // provider: 更新タイミングや表示を指定する
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             if #available(iOS 17.0, *) {
                 WidgetSampleEntryView(entry: entry)
@@ -72,8 +101,30 @@ struct WidgetSample: Widget {
                     .background()
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
+        .configurationDisplayName("Sample Widget")
+        .description("This is an sample widget!!")
+        .supportedFamilies(supportedFamilies)
+    }
+
+    /// iOS16以上で、ロック画面で表示するウィジェットサイズを追加
+    private var supportedFamilies: [WidgetFamily] {
+        if #available(iOSApplicationExtension 16.0, *) {
+            return [
+                .systemSmall,
+                .systemMedium,
+                .systemLarge,
+                .systemExtraLarge,
+                .accessoryInline,
+                .accessoryCircular,
+                .accessoryRectangular
+            ]
+        } else {
+            return [
+                .systemSmall,
+                .systemMedium,
+                .systemLarge
+            ]
+        }
     }
 }
 
